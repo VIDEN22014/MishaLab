77a69f880786ee8530902729fe71fc42fc387c1f
VIDEN22014 <dxbogdan4@gmail.com>
2020-07-18 13:25:05 +0300

    Lab 1 (undone)

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
+#include <vector>
 #include <Windows.h>
 using namespace std;
 
-int main() {
+class vector3d {
+	vector<vector<vector<int>>> vect;
+public:
+	void Init();
+	void Display();
+	
+};
 
+void vector3d::Init(){
+	cout << "¬вед≥ть ширину, довготу ≥ висоту вектора" << endl;
+	int a, b, h;
+	cin >> a >> b >> h;
+	vect.clear();
+	vect.resize(a);
+
+	for (int i = 0; i < a; i++)
+	{
+		vect[i].resize(b);
+
+		for (int j = 0; j < b; j++)
+		{
+			vect[i][j].resize(h);
+
+			for (int k = 0; k < h; k++)
+			{
+				cin >> vect[i][j][k];
+			}
+		}
+	}
+}
+
+void vector3d::Display() {
+	for (int i = 0; i < vect.size(); i++)
+	{
+		for (int j = 0; j < vect[i].size(); j++)
+		{
+			for (int k = 0; k < vect[i][j].size(); k++)
+			{
+				cout << vect[i][j][k] << " ";
+			}
+		}
+	}
+}
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	vector3d v;
+	v.Init();
+	v.Display();
 	return 0;
 }
