159987dab7102ccb2f72e9049504a8ea4375e31a
Микола Максимович <mykola.n1@mail.ru>
2020-07-20 17:56:46 +0300

    new

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,56 +1,132 @@
+/*
+¬ар≥ант 12. —творити клас Aqueue Ц структура типу черга, що базуЇтьс€ на масив≥ покажчик≥в ф≥ксованого розм≥ру.
+ѕередбачити, щоб черга мала можлив≥сть вм≥щувати значенн€ типу, що в≥дпов≥даЇ б≥бл≥отечному класу string.
+ѕередбачити функц≥њ Ц члени класу дл€ виконанн€ таких операц≥й:
+аdd() Ц занести у чергу надане значенн€;
+pop() Ц добути ≥ вилучити значенн€ ≥з черги;
+print() Ц вивести ус≥ значенн€ ≥з черги на екран;
+num() Ц повернути к≥льк≥сть значень, що знаход€тьс€ у черз≥;
+isempty() Ц повернути значенн€ true (тип bool), €кщо черга пуста.
+
+\\м≥н€й на св≥й
+*/
+
 #include <iostream>
-#include <vector>
 #include <Windows.h>
 using namespace std;
 
-class vector3d {
-	vector<vector<vector<int>>> vect;
+class aQueue
+{
 public:
-	void Init();
-	void Display();
-	
+	aQueue();
+	aQueue(string x);
+	~aQueue();
+private:
+	class data
+	{
+	public:
+		data(string x)
+		{
+			this->dani = x;
+			this->next = nullptr;
+		}
+		~data()
+		{
+			delete next;
+		}
+		string Getdata() { return dani; }
+		data* next;
+	private:
+		string dani;
+	};
+
+	data* head;
+	data* tail;
+
+public:
+
+	void add(string x)
+	{
+		if (head == nullptr)
+		{
+			tail = head = new data(x);
+		}
+		else
+		{
+			tail->next = new data(x);
+			tail = tail->next;
+		}
+	}
+
+	string pop()
+	{
+		string x = head->Getdata();
+		data* del = head;
+		head = head->next;
+		del->next = nullptr;
+		delete del;
+		return x;
+	}
+
+	void print()
+	{
+		cout << "¬с≥ дан≥ в черз≥:" << endl;
+		data* scroll = head;
+		while (scroll != nullptr)
+		{
+			cout << scroll->Getdata() << " " << endl;
+			scroll = scroll->next;
+		}
+	}
+	int num()
+	{
+		int num = 0;
+		data* scroll = head;
+		while (scroll != nullptr)
+		{
+			num++;
+			scroll = scroll->next;
+		}
+		return num;
+	}
+	bool isempty()
+	{
+		if (head == nullptr)
+		{
+			return true;
+		}
+	}
+
 };
 
-void vector3d::Init(){
-	cout << "¬вед≥ть ширину, довготу ≥ висоту вектора" << endl;
-	int a, b, h;
-	cin >> a >> b >> h;
-	vect.clear();
-	vect.resize(a);
-
-	for (int i = 0; i < a; i++)
-	{
-		vect[i].resize(b);
-
-		for (int j = 0; j < b; j++)
-		{
-			vect[i][j].resize(h);
-
-			for (int k = 0; k < h; k++)
-			{
-				cin >> vect[i][j][k];
-			}
-		}
-	}
+aQueue::aQueue()
+{
+	tail = head = nullptr;
 }
 
-void vector3d::Display() {
-	for (int i = 0; i < vect.size(); i++)
-	{
-		for (int j = 0; j < vect[i].size(); j++)
-		{
-			for (int k = 0; k < vect[i][j].size(); k++)
-			{
-				cout << vect[i][j][k] << " ";
-			}
-		}
-	}
+aQueue::aQueue(string x)
+{
+	tail = head = new data(x);
 }
 
+aQueue::~aQueue()
+{
+	delete head;
+	delete tail;
+}
 
-int main() {
-	vector3d v;
-	v.Init();
-	v.Display();
-	return 0;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	aQueue x;
+	x.add("lol");
+	x.add("x");
+	x.add("z");
+
+	x.print();
+	x.pop();
+	x.print();
+	//cout << "Hello World!\n";
 }
