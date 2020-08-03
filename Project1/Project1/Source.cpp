8e124116e708a1241d8c966eef7e9ba478c5c129
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2020-08-03 13:23:55 +0300

    подрочив

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <vector>
 #include <Windows.h>
 using namespace std;
 
+class Word {
+	int size;
+	char *w;
+public:
+	Word() { size = 1; w = new char[size]; }
+	Word(int k) { if (k < 65535) { size = k; w = new char[size]; } else  Word(); }
+	~Word() { delete[] w; }
+	Word& operator= (Word& A) { strcpy(this->w, A.w); }
+
+};
 
 int main() {
+	Word t(7);
+
 	setlocale(LC_ALL, "ru");
 	
 	return 0;
