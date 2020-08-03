1505739cbded69bc53f454568f95d5f7a7553e95
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2020-08-03 15:07:32 +0300

    подрочив 2.0

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
-#include <Windows.h>
+#include<stdio.h>
+#include<cstring>
 using namespace std;
 
 class Word {
 	int size;
-	char *w;
+	 char *w = new char[1];
 public:
-	Word() { size = 1; w = new char[size]; }
+	Word() {}
 	Word(int k) { if (k < 65535) { size = k; w = new char[size]; } else  Word(); }
-	~Word() { delete[] w; }
-	Word& operator= (Word& A) { strcpy(this->w, A.w); }
+	~Word() { delete[] w;  }
+	Word& operator= (Word& A) { this->w=A.w; }
+	friend istream& operator >>(istream& in, Word& word) 
+	{
 
+		gets_s(word.w);
+	
+		return in;
+
+	}
+	friend ostream& operator <<(ostream& out, Word& wo) { out << "You input: " << wo.w; return out; }
 };
 
 int main() {
 	Word t(7);
+	cin >> t;
+	cout << t;
+	
 
-	setlocale(LC_ALL, "ru");
 	
 	return 0;
 }
