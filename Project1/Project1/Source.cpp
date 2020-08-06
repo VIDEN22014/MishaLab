7241a4c65418ce9ccd991a989e46f86cc7a01e60
Микола Максимович <mykola.n1@mail.ru>
2020-08-06 16:15:24 +0300

    Просто вставив код Макса.

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,57 +1,84 @@
 #include <iostream>
-#include <vector>
-#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <cstdio>
+#include <conio.h>
 using namespace std;
-
-class vector3d {
-	vector<vector<vector<int>>> vect;
+class CFile
+{
+    //FILE* f;// такий тип потрібний для того щоб передавати потік з файлу прмо в буфер вказуюючи його як аргумент в наступних функціях fopen...  
+    fstream f;
+    string name;
+    char* br;
 public:
-	void Init();
-	void Display();
-	
+    virtual void open(string name2)
+    {
+        name = name2;
+        cout << "File open";
+        //f = fopen(name, "r");//відкриває файл суто для запису по замовчуванню текстовий
+        f.open(name);
+    }
+
+
+    void close()
+    {
+        //fclose(f);//закриває файл обовязковий аргумент це тип файл який після закриття файлу не звязаний із потоком(файл не звязаний)
+        f.close();
+    }
+
+    char seek(char* a)
+    {
+        int k = 0;
+        for (int i = 0, j = 0; i < strlen(br); i++) {
+            if (br[i] == a[j]) {
+                j++; k++;
+                if (br[i + 1] != a[j + 1]) {
+                    j--;
+                }
+            }
+            if (strlen(a) == k) { cout << a << endl; }
+        }
+    }
+
+    void read()
+    {
+        ifstream in;
+        //char *br;
+        char ch;
+        int i = 0;
+        while (in.get(ch)) {
+            cout << ch;
+            br[i] = ch;
+            i++;
+        }
+
+    }
+
+    void write()
+    {
+        string k;
+        ofstream out;
+        cout << "Input need string for writing: " << endl;
+        cin >> k;
+        out << k << endl;
+
+    }
+
 };
 
-void vector3d::Init(){
-	cout << "Введіть ширину, довготу і висоту вектора" << endl;
-	int a, b, h;
-	cin >> a >> b >> h;
-	vect.clear();
-	vect.resize(a);
+class CMyDataFile : public CFile
+{
+public:
+    CMyDataFile() {}
+    ~CMyDataFile() {}
 
-	for (int i = 0; i < a; i++)
-	{
-		vect[i].resize(b);
+};
 
-		for (int j = 0; j < b; j++)
-		{
-			vect[i][j].resize(h);
-
-			for (int k = 0; k < h; k++)
-			{
-				cin >> vect[i][j][k];
-			}
-		}
-	}
+void main(void)
+{
+    string g;
+    cin >> g;
+    CMyDataFile a;
+    a.open(g);
 }
-
-void vector3d::Display() {
-	for (int i = 0; i < vect.size(); i++)
-	{
-		for (int j = 0; j < vect[i].size(); j++)
-		{
-			for (int k = 0; k < vect[i][j].size(); k++)
-			{
-				cout << vect[i][j][k] << " ";
-			}
-		}
-	}
-}
-
-
-int main() {
-	setlocale(LC_ALL, "ru");
-	vector3d v;
-	v.Init();
-	v.Display();
-	return 0;
-}
+// Код Максимуса
